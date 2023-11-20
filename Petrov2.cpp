2292f092127ea8d91868dbddfc603a3461111827
PUPUPU228 <151516533+PUPUPU228@users.noreply.github.com>
2023-11-20 13:14:22 +0300

    Add files via upload

---
+++ Petrov2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+
+    int num1;
+    int num2;
+    int result;
+    cout << " Введите первое число: ";
+    cin >> num1;
+    cout << " Введите второе число: ";
+    cin >> num2;
+    cout << "Каков результат умножения этих 2 чисел?";
+    cin >> result;
+
+    if (result == num1 * num2)
+    {
+        cout << " Верно, ты молодец!";
+    }
+    else
+    {
+        cout << "Неверно.";
+    }
+    return 0;
+}
---
+++ Petrov3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int day;
+    cout << "Введите номер недели";
+    cin >> day;
+    if (day == 1)(day == 2);
+    cout << "Отдыхаю" << endl;
+    if (day == 3) (day == 4);
+    cout << "Работаю" << endl;
+    if (day == 5) (day == 6);
+    cout << "Учусь" << endl;
+    if (day == 7);
+    cout << "Отдыхаю" << endl;
+}
